70861a38d35862110d38c911449288325e488e2b
gietema <jochem@giete.ma>
2019-09-29 15:32:38 +0200

    Hide legend of chart

--- assets/js/components/charts/LineChart.js
+++ assets/js/components/charts/LineChart.js
@@ -14,7 +14,10 @@ export default {
             xAxes: [{
                 display: false //this will remove all the x-axis grid lines
             }]
-        }
+        },
+        legend: {
+            display: false
+        },
     })
   }
 }
