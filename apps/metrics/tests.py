eefe2ef8868c269c4317276fee21ae3a9711b832
gietema <jochem@giete.ma>
2019-10-04 20:27:45 +0200

    Add test test

--- apps/metrics/tests.py
+++ apps/metrics/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Metric
+
+class MetricTestCase(TestCase):
+    def setUp(self):
+        self.metric = Metric.objects.create(
+          name="confidence",
+          description="Tracks model confidence"
+        )
+
+    def test_metric_is_created(self):
+        """Metrics are created"""
+        metric1 = Metric.objects.get(name="confidence")
+        self.assertEqual(metric1.description, "Tracks model confidence")
