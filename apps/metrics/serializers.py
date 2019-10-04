43729ae69a7879cfbb910bfad77e32f95cfdaf98
gietema <jochem@giete.ma>
2019-10-04 22:52:08 +0200

    Remove serializer and django restframework reference

--- apps/metrics/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Metric
-
-
-class MetricSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Metric
-        fields = ('id', 'name', 'description')
--- apps/metrics/views.py
+++ apps/metrics/views.py
@@ -3,17 +3,11 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 
-from rest_framework import generics
 
 from .models import Metric
 from users.models import User
-from .serializers import MetricSerializer
 
 
 class ListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'metrics'
     model = Metric
-
-class MetricListCreate(generics.ListCreateAPIView):
-    queryset = Metric.objects.all()
-    serializer_class = MetricSerializer
