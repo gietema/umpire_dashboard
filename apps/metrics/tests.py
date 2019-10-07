ae80417165db5d66eb6436d4975b61e353fd0ced
gietema <jochem@giete.ma>
2019-10-07 23:36:19 +0200

    fix test

--- apps/metrics/tests.py
+++ apps/metrics/tests.py
@@ -9,10 +9,10 @@ class MetricTestCase(TestCase):
 
     def setUp(self):
         self.metric = Metric.objects.create(
-            name="confidence", description="Tracks model confidence"
+            name="test", description="Tracks model confidence"
         )
 
     def test_metric_is_created(self):
         """Metrics are created"""
-        metric1 = Metric.objects.get(name="confidence")
+        metric1 = Metric.objects.get(name="test")
         self.assertEqual(metric1.description, "Tracks model confidence")
