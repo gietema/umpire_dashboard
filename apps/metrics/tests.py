47dca3851176b872c56087330efe9b1e4ba569da
gietema <jochem@giete.ma>
2019-10-23 23:09:53 +0200

    add test

--- apps/metrics/tests.py
+++ apps/metrics/tests.py
@@ -1,6 +1,9 @@
 """Tests for metric"""
+from django.test import Client
 from django.test import TestCase
 
+from users.models import User
+from stats.models import Stat
 from .models import Metric
 
 
@@ -9,10 +12,20 @@ class MetricTestCase(TestCase):
 
     def setUp(self):
         self.metric = Metric.objects.create(
-            name="test", description="Tracks model confidence"
+            name="test", description="Tracks model test"
+        )
+        self.user = User.objects.create(
+            email='foo@bar.com', password="foobar", api_key='123'
         )
 
     def test_metric_is_created(self):
         """Metrics are created"""
         metric1 = Metric.objects.get(name="test")
-        self.assertEqual(metric1.description, "Tracks model confidence")
+        self.assertEqual(metric1.description, "Tracks model test")
+
+    def test_metric_is_saved(self):
+        """Test that metric is saved when posted"""
+        client = Client()
+        response = client.post('/stats/', {'api_key': '123', 'type': 'confidence', 'stat': 1})
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, len(Stat.objects.all()))
